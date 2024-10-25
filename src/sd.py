3bb7483fc214ebe542f22063e14120251122a730
steven yu <jpstevenjpyu@gmail.com>
2024-10-24 23:34:12 -0700

    update name

---
+++ src/sd.py
@@ -0,0 +1,4 @@
+from transformers import pipeline
+pipe = pipeline("text-to-speech", model="suno/bark-small")
+text = "[clears throat] This is a test ... and I just took a long pause."
+output = pipe(text)
--- src/transformers.py
+++
@@ -1,2 +0,0 @@
-from TTS.api import TTS
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
